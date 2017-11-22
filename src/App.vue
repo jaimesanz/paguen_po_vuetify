307440107fcc0088d6c9e463b95a26e150b9fd70
jaimesanz <jimbosanz23@gmail.com>
2017-11-22 19:43:41 -0300

    Changed template. Added script to check which endpoint to use (dev or real endpoint).

--- src/App.vue
+++ src/App.vue
@@ -1,111 +1,55 @@
 <template>
-  <v-app light>
+  <v-app id="inspire" dark>
     <v-navigation-drawer
+      clipped
       fixed
-      :mini-variant="miniVariant"
-      :clipped="clipped"
       v-model="drawer"
       app
     >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          value="true"
-        >
+      <v-list dense>
+        <v-list-tile @click="">
           <v-list-tile-action>
-            <v-icon light v-html="item.icon"></v-icon>
+            <v-icon>dashboard</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar app>
+    <v-toolbar app fixed clipped-left>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-toolbar-title>PaguenPo</v-toolbar-title>
     </v-toolbar>
     <v-content>
-      <v-container fluid>
-        <v-slide-y-transition mode="out-in">
-          <v-layout column align-center>
-            <img src="./assets/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
-          </v-layout>
-        </v-slide-y-transition>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <p>Endpoint es {{api.API_BASE_URL}}</p>
+        </v-layout>
       </v-container>
-      <router-view></router-view>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      fixed
-      :right="right"
-      v-model="rightDrawer"
-      app
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
+    <v-footer app fixed>
       <span>&copy; 2017</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+  import apiendpoint from './apiendpoint';
+
   export default {
     data() {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [{
-          icon: 'bubble_chart',
-          title: 'Inspire',
-        }],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js',
+        drawer: null,
+        api: apiendpoint,
       };
     },
   };
---
+++ src/apiendpoint.js
@@ -0,0 +1,10 @@
+// taken from: http://gkibria.com/2017/03/04/webpack-way-of-using-dynamic-api-endpoints-based-on-environment/
+const IS_LOCAL = process.env.NODE_ENV !== 'production';
+
+const API_BASE_URL = IS_LOCAL ? 'http:localhost:8000' : 'https://paguenpo.cl/api';
+
+const LOGIN_URL = `${API_BASE_URL}/login`;
+
+export default {
+  IS_LOCAL, API_BASE_URL, LOGIN_URL,
+};

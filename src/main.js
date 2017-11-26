d92ccaf0215bcedd33e4f544d40e28147b246288
jaimesanz <jimbosanz23@gmail.com>
2017-11-26 18:55:07 -0300

    Added Vuex to yarn dependencies.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-
+import Vuex from 'vuex';
 import Vuetify from 'vuetify';
+
 import 'vuetify/dist/vuetify.css';
 
 import App from './App';
 import router from './router';
 
 Vue.use(Vuetify);
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
